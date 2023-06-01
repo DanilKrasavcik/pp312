721bc64058d5fa4d47af2285b1e7fd79ebc33225
DanilKrasavcik <dkrasavcik91@gmail.com>
2023-06-01 15:39:37 +0300

    first commit

---
+++ src/main/java/home/danil/myboot/PP312/services/UsersServiceImpl.java
@@ -0,0 +1,56 @@
+package home.danil.myboot.PP312.services;
+
+
+
+import home.danil.myboot.PP312.models.User;
+import home.danil.myboot.PP312.repositories.UsersRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@Transactional(readOnly = true)
+public class UsersServiceImpl implements UsersService {
+
+    private final UsersRepository usersRepository;
+
+    @Autowired
+    public UsersServiceImpl(UsersRepository usersRepository) {
+        this.usersRepository = usersRepository;
+    }
+
+    @Override
+    public List<User> findAll() {
+        return usersRepository.findAll();
+    }
+
+    @Override
+    public User findOne(long id) {
+        Optional<User> foundUser = usersRepository.findById(id);
+        return foundUser.orElse(null);
+    }
+
+    @Override
+    @Transactional
+    public void save(User user) {
+        usersRepository.save(user);
+    }
+
+    @Override
+    @Transactional
+    public void update(long id, User updatedUser) {
+        updatedUser.setId(id);
+        usersRepository.save(updatedUser);
+    }
+
+    @Override
+    @Transactional
+    public void delete(long id) {
+        usersRepository.deleteById(id);
+    }
+
+
+}
